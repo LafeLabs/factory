a0d42089bb5910940d1e1d2eaa62630b39c7d2f5
Lafe Spietz <lafelabs@gmail.com>
2018-09-24 19:55:28 -0400

    very small tweaks to symbol factory

--- symbol/replicator.php
+++ symbol/replicator.php
@@ -1,6 +1,6 @@
 <?php
 
-    $url = "https://raw.githubusercontent.com/LafeLabs/root2/master/symbol/json/dna.txt";
+    $url = "https://raw.githubusercontent.com/LafeLabs/factory/master/symbol/json/dna.txt";
     $dnaraw = file_get_contents($url);
     $dna =json_decode($dnaraw);
     $baseurl = explode("json",$url)[0];
